65718f8ab298358008faeeb3ebc350a7bd378f6e
SShingler <s.shingler@hotmail.co.uk>
2023-02-18 11:29:28 +0000

    3 - set up models

---
+++ models/destination.py
@@ -0,0 +1,8 @@
+class DESTINATION:
+    def __init__(self, destination_name, country, continent, id = None):
+        self.destination_name = destination_name
+        self.country = country
+        self.continent = continent
+        self.id = id
+
+        
