374a4aed4dd6e58145439f6f13b97a33aaf0e475
SShingler <s.shingler@hotmail.co.uk>
2023-02-19 12:03:43 +0000

    8 - set up destination/index.html & treks/index.html

--- controllers/destinations_controller.py
+++ controllers/destinations_controller.py
@@ -7,7 +7,7 @@ import repositories.trek_repository as trek_repository
 
 treks_blueprint = Blueprint("treks", __name__)
 
-#@treks_blueprint.route("/")
-#def destinations():
-#    destinations = destination_repository.select_all()
-#    return render_template ("destinations/index.html", all_destinations = destinations)
+@treks_blueprint.route("/destinations")
+def destinations():
+    destinations = destination_repository.select_all()
+    return render_template ("destinations/index.html", all_destinations = destinations)
